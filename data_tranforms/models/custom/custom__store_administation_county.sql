e09c1ae076224269b9111cbb05e652d15fd3c903
Kimaru thagana <macbuntu@localhost.localdomain>
2021-08-03 17:31:23 +0300

    add store source in models

---
+++ data_tranforms/models/custom/custom__store_administation_county.sql
@@ -0,0 +1,28 @@
+
+with store_data as (
+
+   SELECT * FROM {{ ref('custom__physical_stores') }}
+
+
+)
+
+with county_data as (
+
+   SELECT * FROM {{ ref('county_data') }}
+
+
+)
+
+final as (
+    SELECT store_data.store_name,
+           store_data.store_manager
+           store_data.store_id
+           county_data.county_name
+           county_data.county_population
+           FROM store_data
+           LEFT JOIN 
+            county_data
+            ON store_data.county_code = county_data.county_code
+)
+
+SELECT * FROM final
